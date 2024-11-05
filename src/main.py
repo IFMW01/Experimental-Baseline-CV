ac9120fd1e6b5efb3282941746e71e9a8cf2457a
Israel Francis Mason-Williams <55697240+IFMW01@users.noreply.github.com>
2024-11-05 14:56:47 +0000

    Update main.py
    For deterministic training

--- src/main.py
+++ src/main.py
@@ -52,8 +52,9 @@ def set_seed(seed):
     np.random.seed(seed)
     torch.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
-    # torch.backends.cudnn.deterministic = True
-    # torch.backends.cudnn.benchmark = True
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    # Benchmark is not optimised to be deterministic
 
 
 def get_device():
